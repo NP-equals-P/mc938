9ef57fd7c568696bce83621b3f456f31e0f82281
LuizFC-testes <luiz_entt@outlook.com>
2025-11-26 21:32:10 -0300

    Módulo personalizado atualizado para melhorar aleatoriedade 2

--- CustomGenerator/byte_search.py
+++ CustomGenerator/byte_search.py
@@ -27,21 +27,35 @@ def getByteChain(text : str, numBytes : int):
     byteChain = ""
     idx = 0
     max_idx = len(text) - 1
+    i_1, i_2, i_3 = -1, -2, -4
     while remaining_bytes > 0:
         net_read = net_io_counters()
         bytesOut, bytesIn = net_read.bytes_sent, net_read.bytes_recv
-        idx += (-1)**(bytesOut%10) * (bytesIn%1000)
+        # Embaralha os dígitos de bytesOut
+        for i in [i_1, i_2, i_3]:
+            bytesOut = proc.swapBits(str(bytesOut), i, -i)
+        bytesOut = int(bytesOut)
+        # Atualiza os dígitos sorteados e os índices dos próximos dígitos
+        i_1, d_1 = proc.updateDigit(bytesOut, i_1, i_3-i_2)
+        i_2, d_2 = proc.updateDigit(bytesOut, i_2, i_1-i_3)
+        i_3, d_3 = proc.updateDigit(bytesOut, i_3, i_2-i_1)
+        # Atualiza o 'cursor' do byte a ser obtido da string ('text')
+        idx += (-1)**(d_1) * (bytesIn%1000)
         idx = idx % max_idx # Ou len(text) ?
-        curr_chain = getChainFromTxt(text, idx)
+        curr_chain = getChainFromTxt(text, idx) # Obtém o byte a partir de 'text'
         # Último dígito
-        if bytesOut%2 != 0:
+        if d_1%2 != 0:
             curr_chain = '1' + curr_chain[1:] # Troca o primeiro dígito por '1'
         # Penúltimo dígito
-        if ((bytesOut%100)//10)%2 != 0:
+        if d_2%2 != 0:
             curr_chain = proc.reverseString(curr_chain) # Inverte a ordem dos bits
         # Antepenúltimo dígito
-        if ((bytesOut%1000)//100)%2 != 0:
+        if d_3%2 != 0:
             curr_chain = proc.invertBits(curr_chain) # Troca a 'polaridade' de cada bit
-        byteChain += proc.tabBinByte(curr_chain)
+        curr_chain = proc.tabBinByte(curr_chain)
+        # Troca aleatória de bits
+        curr_chain = proc.swapBits(curr_chain, d_1, d_2)
+        byteChain += curr_chain
         remaining_bytes -= 1
     return byteChain
+        
--- CustomGenerator/processing.py
+++ CustomGenerator/processing.py
@@ -44,3 +44,44 @@ def invertBits(n):
     operand2 = '1'*len(n)
     n_inverted = int(n, 2) ^ int(operand2, 2)
     return bin(n_inverted)[2:]
+
+def swapBits(b, idx_1, idx_2):
+    """
+    * Swaps bits from a binary number between given positions
+    * Parameters:
+    * * b (str) - The binary number, without '0b'
+    * * idx_1, idx_2 (int) - Positions to be swapped
+    """
+    idx_1, idx_2 = idx_1%len(b), idx_2%len(b)
+    if idx_1 > idx_2:
+        idx_1, idx_2 = idx_2, idx_1
+    bit1, bit2 = b[idx_1], b[idx_2]
+    if idx_1 != idx_2:
+        return b[:idx_1] + bit2 + b[idx_1+1:idx_2] + bit1 + b[idx_2+1:]
+    return b
+
+def selectDigit(n, pos : int):
+    """
+    * Returns the digit of an integer at a specified position, as if it was a string
+    * Parameters:
+    * * n (int)
+    * * pos (int) - Position of the digit.
+    """
+    n_str = str(n)
+    digit = n_str[pos%len(n_str)]
+    return int(digit)
+
+def updateDigit(n, idx, idx_aux):
+    """
+    * Gets the digit of an integer 'n' at a specified position and the next position to be used
+    * Parameters:
+    * * n (int)
+    * * idx (int) - Position of the digit
+    * * idx_aux (int) - Position of an auxiliary digit to be used in the calculation of the next index
+    * Returns:
+    * * next_idx (int) - Index of the next iteration
+    * * digit (int)  - Digit for the current iteration
+    """
+    digit = selectDigit(n, idx)
+    aux = selectDigit(n, idx_aux)
+    return (idx-digit)%len(str(n)) - aux + ((aux%2)==0), digit
